10809d99e294c2476e7a0966f9d1fc5a4dd6e77b
pablo <digounet@gmail.com>
2017-08-05 00:14:44 -0300

    Error on start EndGameViewController

--- FizzBuzz/FizzBuzz/EndGameViewController.swift
+++ FizzBuzz/FizzBuzz/EndGameViewController.swift
@@ -17,7 +17,7 @@ class EndGameViewController: BaseViewController {
     internal  var hasNoPlaceHolder = false
     
     private let imagePicker = UIImagePickerController()
-    private var confettiView: SAConfettiView!
+    private var confettiView: SAConfettiView?
     
     @IBOutlet weak var lblScore: UILabel!
     @IBOutlet weak var imgPlayerPhoto: UIImageView!
@@ -42,30 +42,30 @@ class EndGameViewController: BaseViewController {
             confettiView = SAConfettiView(frame: self.view.bounds)
             
             // Set colors (default colors are red, green and blue)
-            confettiView.colors = [UIColor(red:0.95, green:0.40, blue:0.27, alpha:1.0),
+            confettiView?.colors = [UIColor(red:0.95, green:0.40, blue:0.27, alpha:1.0),
                                    UIColor(red:1.00, green:0.78, blue:0.36, alpha:1.0),
                                    UIColor(red:0.48, green:0.78, blue:0.64, alpha:1.0),
                                    UIColor(red:0.30, green:0.76, blue:0.85, alpha:1.0),
                                    UIColor(red:0.58, green:0.39, blue:0.55, alpha:1.0)]
             
             // Set intensity (from 0 - 1, default intensity is 0.5)
-            confettiView.intensity = 0.5
+            confettiView?.intensity = 0.5
             
             // Set type
-            confettiView.type = .confetti
+            confettiView?.type = .confetti
             
             
             // Add subview
-            view.addSubview(confettiView)
-            confettiView.startConfetti()
+            view.addSubview(confettiView!)
+            confettiView?.startConfetti()
         }
         
     }
     
     func stopConfetti() {
-        if confettiView.isActive() {
-            self.confettiView.stopConfetti()
-            self.confettiView.removeFromSuperview()
+        if confettiView != nil && (confettiView?.isActive())! {
+            self.confettiView?.stopConfetti()
+            self.confettiView?.removeFromSuperview()
         }
     }
     
